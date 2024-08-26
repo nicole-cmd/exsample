60c9cb44bcfb11bbf7d2d5c4f2993542fedd46b9
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-08-26 09:31:36 -0500

    Fix Checkstyle violations.

--- src/main/java/edu/grinnell/Sample.java
+++ src/main/java/edu/grinnell/Sample.java
@@ -6,10 +6,13 @@ import java.io.PrintWriter;
  * A sample class.
  */
 public class Sample {
-  /** 
+  /**
    * Do something boring, such as printing a message.
-   */ 
-   public static void main(String[] args) {
+   *
+   * @param args
+   *   The array of command-line arguments.
+   */
+  public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println("Hello Sample");
     pen.close();
